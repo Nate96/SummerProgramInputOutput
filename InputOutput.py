dd768db262acc368f845210da653e04e50b035bf
Nate Braukhoff <braukhoff96@gmail.com>
2018-06-26 14:41:35 -0500

    Finalized the workshop deleted all the code that the kids will be filling out

--- InputOutput.py
+++ InputOutput.py
@@ -1,5 +1,5 @@
-# In this excise we will be learning how to input and output data
-# from the terminal with Python.
+# Purpose: In this excise we will be learning how to input and
+#          output data from the terminal with Python.
 
 # Things to remember: print(), input()
 # Symbols: number): steps for complete
@@ -9,24 +9,26 @@
 class InputOutput:
     # 1): Declare 2 *global variables* and name them number1
     #     and number2 and set them to 0
-    number1 = 0
-    number2 = 0
 
     # Method that will get Input from the user
+    # 2): Use the input() method in the int() to get input from the
+    #     user
+    #     hint: don't forget to use the "" in input
     def getInput(self):
-        self.number1 = int(input("Enter in the first number: "))
-        self.number2 = int(input("Enter in the second number: "))
+        self.number1 = int()
+        self.number2 = int()
 
     # Method that will add the two number together
-    # 2): add the x and y together and *return* the sum
+    # 3): add the x and y together and *return* the sum
     def add(self, x, y):
-        return x + y
+        return
 
     # Method that will get two numbers from the user and add them
     # together
     def run(self):
         self.getInput()
-        return self.add(self.number1, self.number2)
+        print("The total of the numbers are: {} "
+              .format(self.add(self.number1, self.number2)))
 
 
 if __name__ == "__main__":
--- Tests/InputOutputTest.py
+++ Tests/InputOutputTest.py
@@ -1,24 +1,33 @@
 import unittest
 from InputOutput import InputOutput
 
-class TestInputOuput(unittest.TestCase):
+
+class TestInputOutput(unittest.TestCase):
     def setUp(self):
         self.InputOutput = InputOutput()
 
+    # This will test getInput method.
+    # To run the test click on the little green arrow on the left
+    # next to the line number.
     def test_getInput(self):
         self.InputOutput.getInput()
 
+        # Test 1: Enter in 1 for the first number and
+        # 2 for the second number.
         self.assertEqual(1, self.InputOutput.number1)
         self.assertEqual(2, self.InputOutput.number2)
 
         self.InputOutput.getInput()
 
+        # Test 2: enter in 45 for the first number and
+        # 100 for the second number
         self.assertEqual(45, self.InputOutput.number1)
         self.assertEqual(100, self.InputOutput.number2)
 
+    # This will test your add method
     def test_add(self):
         self.assertEqual(3, self.InputOutput.add(1, 2))
         self.assertEqual(5, self.InputOutput.add(-5, 10))
 
     def test_run(self):
-        self.assertEqual(10, self.InputOutput.run())
+        self.assertEqual (10, self.InputOutput.run())
